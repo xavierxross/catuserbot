ffe65545fe28189fad1930703677ce09632b8898
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-07 18:49:22 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -24,47 +24,25 @@ echo "
 :.......::::......:::........::..:::::..::........::::.......::::::..:::::
 "
 
-if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
-    herokuErr=$(python ./.github/herokugiturl.py)
-    if [[ "$herokuErr" ]]; then 
-        echo "$herokuErr"
-    else
-        HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
-        echo "$HEROKU_GIT_URL"
-    fi
-fi
-
 FILE=/app/.git
 
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    if [[ "$HEROKU_GIT_URL" ]]; then
-        {
-          git clone "$HEROKU_GIT_URL" cat_ubh
-          mv cat_ubh/.git .
-          rm -rf cat_ubh
-        } || {
-          git init
-        }
-    else
-        git init
-    fi
+    git clone https://github.com/sandy1709/catuserbot cat_ub
+    rm -rf userbot
+    mv cat_ub/.git .
+    mv cat_ub/userbot .
+    mv cat_ub/requirements.txt .
+    rm -rf cat_ub
+    python ./.github/update.py
 fi
 
 FILE=/app/bin/
-
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    mkdir /app/bin/
-    # downloading bins
-    wget -O /app/bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
-    wget -O /app/bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
-    # changing bins permissions
-    chmod 755 bin/megadown
-    chmod 755 bin/cmrudl 
-    echo "Succesfully bins are added"
+    bash ./.github/bins.sh
 fi
 
 python -m userbot
