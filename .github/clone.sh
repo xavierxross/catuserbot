45fec0c9bd01a87196549b196d4bf6a431a01ff3
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-07 13:55:28 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -24,18 +24,43 @@ echo "
 :.......::::......:::........::..:::::..::........::::.......::::::..:::::
 "
 
+if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
+    herokuErr=$(python ./.github/herokugiturl.py)
+    if [[ "$herokuErr" ]]; then 
+        echo "$herokuErr"
+    else
+        HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
+    fi
+fi
+
 FILE=/app/.git
 
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    git clone -b bugs https://github.com/sandy1709/catuserbot cat_ub
-    rm -rf userbot
-    mv cat_ub/.git .
-    mv cat_ub/userbot .
-    mv cat_ub/requirements.txt .
-    rm -rf cat_ub
-    python ./.github/update.py
+    if [[ "$HEROKU_GIT_URL" ]]; then
+        {
+          git clone "$HEROKU_GIT_URL" cat_ubh
+          mv cat_ubh/.git .
+          rm -rf cat_ubh
+        } || {
+          git clone -b bugs https://github.com/sandy1709/catuserbot cat_ubc
+          rm -rf userbot
+          mv cat_ub/.git .
+          mv cat_ub/userbot .
+          mv cat_ub/requirements.txt .
+          rm -rf cat_ub
+          python ./.github/update.py
+        }
+    else
+        git clone -b bugs https://github.com/sandy1709/catuserbot cat_ub
+        rm -rf userbot
+        mv cat_ub/.git .
+        mv cat_ub/userbot .
+        mv cat_ub/requirements.txt .
+        rm -rf cat_ub
+        python ./.github/update.py
+    fi
 fi
 
 FILE=/app/bin/
