ceb8eaadaef73b75f43af8b22b992d58d7be5035
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-07 18:58:49 +0530

    Update bins.sh

--- .github/bins.sh
+++ .github/bins.sh
@@ -2,8 +2,8 @@
 
 mkdir /app/bin/
 # downloading bins
-wget -O /app/bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
-wget -O /app/bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
+wget -q -O /app/bin/megadown https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown
+wget -q -O /app/bin/cmrudl https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py
 # changing bins permissions
 chmod 755 bin/megadown
 chmod 755 bin/cmrudl 
