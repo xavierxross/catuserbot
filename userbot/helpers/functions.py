f19519016f9caeeedc371c6f43a2f6c0031c6be2
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-09 20:24:07 +0000

    pylint: auto fixes

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -155,7 +155,9 @@ async def extract_time(cat, time_val):
 song_dl = "youtube-dl --force-ipv4 --write-thumbnail -o './temp/%(title)s.%(ext)s' --extract-audio --audio-format mp3 --audio-quality {QUALITY} {video_link}"
 thumb_dl = "youtube-dl --force-ipv4 -o './temp/%(title)s.%(ext)s' --write-thumbnail --skip-download {video_link}"
 video_dl = "youtube-dl --force-ipv4 --write-thumbnail  -o './temp/%(title)s.%(ext)s' -f '[filesize<20M]' {video_link}"
-name_dl = "youtube-dl --force-ipv4 --get-filename -o './temp/%(title)s.%(ext)s' {video_link}"
+name_dl = (
+    "youtube-dl --force-ipv4 --get-filename -o './temp/%(title)s.%(ext)s' {video_link}"
+)
 
 EMOJI_PATTERN = re.compile(
     "["
