d5ed8747d40b71570e4fd83722205fda82fdc7e4
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-11 22:54:53 +0530

    Update antispambot.py

--- userbot/plugins/antispambot.py
+++ userbot/plugins/antispambot.py
@@ -6,19 +6,18 @@
 from requests import get
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-
+from .sql_helper.gban_sql_helper import is_gbanned, get_gbanuser
 from ..utils import is_admin
 from . import BOTLOG, BOTLOG_CHATID, LOGS, spamwatch
 
 if Config.ANTISPAMBOT_BAN:
-
     @bot.on(events.ChatAction())
-    async def _(event):
+    async def antispam_ban(event):
         if not event.user_joined and not event.user_added:
             return
         chat = event.chat_id
         user = await event.get_user()
-        catadmin = await is_admin(bot, chat, bot.uid)
+        catadmin = await is_admin(bot, chat, bot.uid) 
         if not catadmin:
             return
         catbanned = None
@@ -34,42 +33,57 @@ if Config.ANTISPAMBOT_BAN:
                 event.chat_id, filter=ChannelParticipantsAdmins
             )
         )
-
         if ignore:
             return
-        if spamwatch:
-            ban = spamwatch.get_ban(user.id)
-            if ban:
+        if is_gbanned(user.id):
+            catgban = get_gbanuser(user.id)
+            if catgban.reason:
                 hmm = await event.reply(
-                    f"[{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
-                )
+                        f"[{user.first_name}](tg://user?id={user.id}) was gbanned by you For the reason `{catgban.reason}`"
+                    )
+            else:
+                hmm = await event.reply(
+                        f"[{user.first_name}](tg://user?id={user.id}) was gbanned by you"
+                    )
                 try:
                     await bot.edit_permissions(chat, user.id, view_messages=False)
                     catbanned = True
                 except Exception as e:
                     LOGS.info(e)
+        if spamwatch:
             if not catbanned:
-                try:
-                    casurl = "https://api.cas.chat/check?user_id={}".format(user.id)
-                    data = get(casurl).json()
-                except Exception as e:
-                    LOGS.info(e)
-                    data = None
-                if data and data["ok"]:
-                    reason = f"[Banned by Combot Anti Spam](https://cas.chat/query?u={user.id})"
+                ban = spamwatch.get_ban(user.id)
+                if ban:
                     hmm = await event.reply(
-                        f"[{user.first_name}](tg://user?id={user.id}) was banned by Combat anti-spam service(CAS) For the reason check {reason}"
+                        f"[{user.first_name}](tg://user?id={user.id}) was banned by spamwatch For the reason `{ban.reason}`"
                     )
                     try:
                         await bot.edit_permissions(chat, user.id, view_messages=False)
                         catbanned = True
                     except Exception as e:
                         LOGS.info(e)
-            if BOTLOG and catbanned:
-                await bot.send_message(
-                    BOTLOG_CHATID,
-                    "#ANTISPAMBOT\n"
-                    f"**User :** [{user.first_name}](tg://user?id={user.id})\n"
-                    f"**Chat :** {event.chat.title} (`{event.chat_id}`)\n"
-                    f"**Reason :** {hmm.text}",
+        if not catbanned:
+            try:
+                casurl = "https://api.cas.chat/check?user_id={}".format(user.id)
+                data = get(casurl).json()
+            except Exception as e:
+                LOGS.info(e)
+                data = None
+            if data and data["ok"]:
+                reason = f"[Banned by Combot Anti Spam](https://cas.chat/query?u={user.id})"
+                hmm = await event.reply(
+                    f"[{user.first_name}](tg://user?id={user.id}) was banned by Combat anti-spam service(CAS) For the reason check {reason}"
                 )
+                try:
+                    await bot.edit_permissions(chat, user.id, view_messages=False)
+                    catbanned = True
+                except Exception as e:
+                    LOGS.info(e)
+        if BOTLOG and catbanned:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                "#ANTISPAMBOT\n"
+                f"**User :** [{user.first_name}](tg://user?id={user.id})\n"
+                f"**Chat :** {event.chat.title} (`{event.chat_id}`)\n"
+                f"**Reason :** {hmm.text}",
+            )
