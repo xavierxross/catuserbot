56d7cae71fc758f3eaa59eb4bd70e1fdccf2c075
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-12 13:24:15 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -174,18 +174,21 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+)")))
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
     async def on_plug_in_callback_query_handler(event):
         timestamp = event.pattern_match.group(1)
         if os.path.exists("./userbot/secret.txt"):
             jsondata = json.load(open("./userbot/secret.txt"))
             message = jsondata[timestamp]
-            ids = [int(message[0]), bot.uid]
-            if event.query.user_id in ids:
-                encrypted_tcxt = message[1]
-                reply_pop_up_alert = encrypted_tcxt
+            if message:
+                ids = [int(message[0]), bot.uid]
+                if event.query.user_id in ids:
+                    encrypted_tcxt = message[1]
+                    reply_pop_up_alert = encrypted_tcxt
+                else:
+                    reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
             else:
-                reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+                reply_pop_up_alert = "This message no longer exists "
         else:
             reply_pop_up_alert = "This message no longer exists "
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
